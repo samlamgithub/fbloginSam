67011492970c84590b560d06edb5bfc9cbdfb32c
Sam <samlam2012@hotmail.com>
2015-06-01 13:08:50 +0100

    sample fb log in in iOS

---
+++ facebooklogin/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  facebooklogin
+//
+//  Created by Jiahao Lin on 01/06/2015.
+//  Copyright (c) 2015 Jiahao Lin. All rights reserved.
+//
+
+#ifndef facebooklogin_Bridging_Header_h
+#define facebooklogin_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
